9d3da05eaa4746ad5d90565dd84bba108b51c2e0
Kang Hidro <mr.lipgs@gmail.com>
2020-12-10 22:12:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ chrome.webRequest.onBeforeRequest.addListener(
   {
     urls: [
 	  "*://*.chat.zalo.me/api/message/seen*",
-	  "*://*.chat.zalo.me/api/message/typing*"
+	  "*://*.chat.zalo.me/api/message/typing*",
+	  "*://*.chat.zalo.me/api/group/seen*",
+	  "*://*.chat.zalo.me/api/social/profile/ping*",
+	  "*://*.chat.zalo.me/keepalive*"
     ],
     types: ["csp_report", "font", "image", "main_frame", "media", "object", "other", "ping", "script", "stylesheet", "sub_frame", "websocket", "xmlhttprequest"]
   },
